658af8433cf84b83fcce99c9a6fced43eaed9762
Faria-Lorin <62440537+Faria-Lorin@users.noreply.github.com>
2020-10-20 02:43:21 -0400

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class Demo{
 public static void printLoop(int n){
     for(int i = 1; i <= n; i++){
       for(int j = i; j <= n; j++){
-         System.out.print(i); //prints out rectangular shaped numbers (like the example above)
+         System.out.print(i);
       }
       System.out.println();
     }
