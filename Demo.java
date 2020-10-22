bf82447d4abbc2515c1332ce1c4983d3cae9d188
Faria-Lorin <florin20@stuy.edu>
2020-10-22 03:21:26 -0400

    I wrote arrayToString and tested it out and it worked

--- Demo.java
+++ Demo.java
@@ -1,17 +1,42 @@
 public class Demo{
-public static void printLoop(int n){
+  public static void printLoop(int n){
     for(int i = 1; i <= n; i++){
       for(int j = i; j <= n; j++){
          System.out.print(i);
-      }
       System.out.println();
     }
   }
-public static void main(String[] args) {
-  if (args.length == 0){
-    Demo.printLoop(5);
-  }
-  Demo.printLoop(Integer.parseInt(args[0]));
-
-  }
 }
+  public static String arrToString(int[]arr){
+    String sum = "";
+    int n = 0;
+    if (arr.length == 0){
+        sum += "{}";
+      }
+    for (int i = 0; i < arr.length; i++){
+        n = arr[i];
+        if ( i == 0){
+            sum += "{"+ n +", ";
+          }
+        else if ( i != arr.length - 1) {
+            sum += n + ", ";
+          }
+        else {
+            sum += n + "}";
+          }
+        }
+    if (arr.length == 1){
+        sum = "{" + arr[0] + "}";
+      }
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    if (args.length == 0){
+      Demo.printLoop(5);
+    }
+    Demo.printLoop(Integer.parseInt(args[0]));
+    int[] arr = {1,2,3,4,5};
+    System.out.println(Demo.arrToString(arr));
+    }
+  }
